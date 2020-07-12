a48e7ab53eb723fba4a18464adb961cc17f59d88
song <songsm5504@gmail.com>
2020-07-12 22:22:25 +0900

    #2.1 More Function Fun

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-function sayHello(name, age) {
-  console.log("Hello", name, "you have", age, "years of age.");
-}
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  powerof: function (a, b) {
+    return a ** b;
+  },
+};
 
-sayHello("Nicolas", 15);
+// console.log(greetNicolas);
+
+let num1 = 12;
+let num2 = 3;
+
+const calculatorResult = {
+  pluse: calculator.plus(num1, num2),
+  minus: calculator.minus(num1, num2),
+  multiply: calculator.multiply(num1, num2),
+  divide: calculator.divide(num1, num2),
+  powerof: calculator.powerof(num1, num2),
+};
+
+console.log(` pluse : ${calculatorResult.pluse}`);
+console.log(` minus : ${calculatorResult.minus}`);
+console.log(` multiply : ${calculatorResult.multiply}`);
+console.log(` divide : ${calculatorResult.divide}`);
+console.log(` powerof : ${calculatorResult.powerof}`);
