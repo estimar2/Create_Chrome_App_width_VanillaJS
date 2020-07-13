0313169824f330942e0a8a977f9661c8c578e31d
song <songsm5504@gmail.com>
2020-07-14 01:44:43 +0900

    #2.3 Modifying the DOM with JS

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const title = document.getElementById("title");
+const title = document.querySelector("#title");
 
 title.innerHTML = "Hi! From JS";
 
+title.style.color = "yellow";
+
+document.title = "I own you now";
+
 console.log(title);
+
+console.dir(document);
