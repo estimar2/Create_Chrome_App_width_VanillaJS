ee2d46f46b84b7935cc0cdea1f271fbc3207b741
song <songsm5504@gmail.com>
2020-07-14 13:57:17 +0900

    #2.6 DOM - if else - Function practice

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const age = prompt("How old are you");
+const title = document.querySelector("#title");
 
-if (age >= 18 && age <= 21) {
-  console.log("You can drink but you should not");
-} else if (age > 21) {
-  console.log("Go ahead");
-} else {
-  console.log("Too young");
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7ed6df";
+
+function handleClick() {
+  const currentColor = title.style.color;
+
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
+
+function init() {
+  title.style.color = BASE_COLOR;
+}
+
+title.addEventListener("click", handleClick);
+
+init();
