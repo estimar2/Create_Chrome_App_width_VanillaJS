22f74e03096a2381f9fbc6520cc9b19141726122
song <songsm5504@gmail.com>
2020-07-14 13:28:28 +0900

    #2.4 Events and event handlers

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const title = document.querySelector("#title");
 
-title.innerHTML = "Hi! From JS";
+function handleClick() {
+  title.style.color = "blue";
+}
 
-title.style.color = "yellow";
-
-document.title = "I own you now";
-
-console.log(title);
-
-console.dir(document);
+title.addEventListener("click", handleClick);
